f86aa12c9a9f948bea5b7ae6dc00f99af8aedfaf
Simara Conceição <contato@simaraconceicao.com>
2020-06-22 18:25:44 -0300

    adiciona arquivos iniciais

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('entrei')
+
+
+let card = document.getElementById("card")
+
+console.log(card)
+    
+card.addEventListener("click", function (e) {
+    // console.log("estou clicando")
+    card.style.backgroundColor = "green"
+    // alert("você me clicou?")
+    // prompt('qual seu nome?')
+    
+})
+
+
+
